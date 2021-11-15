2f9f59eb2710e8c4ffb1e3a971f2ee3f80fa361f
BTPNSSHIFTED <wiladhianty@gmail.com>
2021-11-15 12:00:29 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  const [greet, setGreet] = useState("");
 
+  useEffect(() => {
+    fetch(
+      "https://heroku:${{secret.HEROKU_API_KEY}}@git.heroku.com/${{secret.HEROKU_APP_NAME}}"
+    ).then((res) => {
+      setGreet(res.data);
+    });
+  }, []);
+
+  return <h1>{greet}</h1>;
+}
 export default App;
